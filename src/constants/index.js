7b68351bb861989abfcb91c4564ad5f685145a8c
Jessica Negri <jessinegri.jn@gmail.com>
2026-08-03 16:53:34 -0300

    primera pre entrega

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+export const USER_ROLES = Object.freeze ({
+    ADMIN: 'admin',
+    CUSTOMER: 'customer',
+    STORE: 'store'
+});
+
+export const ORDER_STATUS = Object.freeze ({
+
+    CREATED: 'created',
+
+    ASSIGNED: 'assigned',
+
+    PICKED_UP: 'picked_up',
+
+    IN_TRANSIT: 'in_transit',
+
+    DELIVERED: 'delivered',
+
+    CANCELLED: 'cancelled'
+
+});
